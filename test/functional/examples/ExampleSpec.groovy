f880874077c1929bac5c630ebba2983103eba54b
Rob Fletcher <rob@serenity.(none)>
2010-11-24 13:16:32 +0000

    simple table example

--- test/functional/examples/ExampleSpec.groovy
+++ test/functional/examples/ExampleSpec.groovy
@@ -2,7 +2,7 @@ package examples
 
 import grails.plugin.geb.GebSpec
 
-class SimpleListSpec extends GebSpec {
+class ExampleSpec extends GebSpec {
 
 	def "confirm contents of a list"() {
 		given:
@@ -11,5 +11,16 @@ class SimpleListSpec extends GebSpec {
 		expect:
 		recentBooks == ["Zero History", "Surface Detail", "The Machine of Death"]
 		popularBooks == ["The Girl With The Dragon Tattoo", "The Girl Who Played With Fire", "The Girl Who Kicked The Hornet's Nest"]
+	}
+
+	def "confirm contents of a table"() {
+		given:
+		to IndexPage
+
+		expect:
+		booksByAuthor(0).title == "Zero History"
+		booksByAuthor(1).title == "Zero History"
+		booksByAuthor(2).title == "Spook Country"
+		booksByAuthor(3).title == "Pattern Recognition"
 	}
 }
--- test/functional/examples/IndexPage.groovy
+++ test/functional/examples/IndexPage.groovy
@@ -11,6 +11,22 @@ class IndexPage extends Page {
 	static content = {
 		recentBooks { $("#recent-books ol li")*.text() }
 		popularBooks { $("#popular-books ol li")*.text() }
+		booksByAuthor { i ->
+			module BookRow, $("#books-by-author table tbody tr", i)
+		}
+	}
+
+}
+
+class BookRow extends Module {
+
+	static content = {
+		cell { i -> $("td", i) }
+		title { cell(0).text() }
+		author { cell(1).text() }
+		format { cell(2).text() }
+		price { cell(3).text() }
+		releaseDate { cell(4).text() }
 	}
 
 }
